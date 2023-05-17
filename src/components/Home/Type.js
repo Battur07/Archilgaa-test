66036e4694cc70faa7f928aa5c192a1b78e27a99
Battur07 <112911353+Battur07@users.noreply.github.com>
2023-05-18 00:05:39 +0900

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Battur",
+          "Bayarjargal",
+          "Ariuka",
+          "Khulan",
         ],
         autoStart: true,
         loop: true,
